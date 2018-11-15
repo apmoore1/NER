f757585a1e903ddb03ed4bea1c714359d18f9aa5
Andrew Moore <andrew.p.moore94@gmail.com>
2018-11-15 18:26:38 +0000

    created train, dev, test split methods

--- creating_data_sets.py
+++ creating_data_sets.py
@@ -53,61 +53,115 @@ def read_data(data_fp: Path) -> List[Tuple[str, str]]:
             sentence_words.append(word)
             sentence_ner_labels.append(ner_label)
     return all_sentence_data
+
+def num_unique_labels(sentence_label_data: List[Tuple[str, str]]
+                      ) -> int:
+    '''
+    Given the output of the :py:func:`read_data` function it returns 
+    the number of unique labels within the data.
+
+    :param sentence_label_data: A list of tuples where the first 
+                                String is the sentence and the 
+                                second are the related word level 
+                                labels
+    :returns: The number of unique labels.
+    '''
+    labels = set()
+    for sentence, joined_labels in sentence_label_data:
+        split_labels = joined_labels.split()
+        for label in split_labels:
+            labels.add(label)
+    return len(labels)
+
+def shuffle_data(sentence_label_data: List[Tuple[str, str]],
+                 test_size: float = 0.2, dev_size: float = 0.2,
+                 random_state: 'np.random.RandomState' = np.random.RandomState()
+                 ) -> Tuple[List[Tuple[str, str]], List[Tuple[str, str]], List[Tuple[str, str]]]:
+    '''
+    Given the output of :py:func:`read_data` it will split the data 
+    into random train, dev, test splits.
+
+    :param sentence_label_data: A list of tuples where the first 
+                                String is the sentence and the 
+                                second are the related word level 
+                                labels
+    :param test_size: The size of the test data in relation to the 
+                      whole dataset
+    :param dev_size: The size of the dev data in relation to the 
+                     data after it has been split into train and test
+                     and this would come from the train set not the 
+                     test.
+    :param random_state: Whether or not the process should be repeatable
+                         if so specify a number e.g. 42 else leave to 
+                         default.
+    :returns: A Tuple of (train, dev, test) where the data is in the 
+              same format as the input.
+    '''
+
+    num_classes = num_unique_labels(sentence_label_data)
+        
+    train, test = train_test_split(sentence_label_data, 
+                                   test_size=test_size,
+                                   random_state=random_state)
+    train, dev = train_test_split(train, test_size=dev_size,
+                                  random_state=random_state)
+    for dataset in [train, dev, test]:
+        if num_unique_labels(dataset) != num_classes:
+            return shuffle_data(sentence_label_data, test_size, 
+                                dev_size, random_state)
+    return train, dev, test
+
+def data_to_file(sentence_label_data: List[Tuple[str, str]],
+                 data_fp: Path) -> None:
+    '''
+    Write the data to the given file in CoNLL 2003 format.
+
+    :param sentence_label_data: A list of tuples where the first 
+                                String is the sentence and the 
+                                second are the related word level 
+                                labels
+    :param data_fp: File to write the data to.
+    :returns: Nothing
+    '''
+    with data_fp.open('w+') as data_file:
+        for index, sentence_joined_labels in enumerate(sentence_label_data):
+            sentence, joined_labels = sentence_joined_labels
+            words = sentence.split()
+            labels = joined_labels.split()
+
+            num_words = len(words)
+            num_labels = len(labels)
+            if num_words != num_labels:
+                raise ValueError(f'The number of words {num_words} should be the '
+                                 f'same as the number of labels {num_labels}')
+            for word_index, word in enumerate(words):
+                label = labels[word_index]
+                if index == 0 and word_index == 0:
+                    data_file.write(f'{word} -X- -X- {label}')
+                else:
+                    data_file.write(f'\n{word} -X- -X- {label}')
+            data_file.write('\n')
             
 if __name__ == '__main__':
-    data_dir = Path('/', 'home', 'andrew', 'Documents', 'conll_2003')
+    data_dir = Path('..', 'conll_2003')
     train_data = read_data(Path(data_dir, 'train.txt'))
     assert len(train_data) == 14041
     dev_data = read_data(Path(data_dir, 'dev.txt'))
     assert len(dev_data) == 3250
     test_data = read_data(Path(data_dir, 'test.txt'))
     assert len(test_data) == 3453
+    all_data = train_data + dev_data + test_data
+    assert len(all_data) == (14041 + 3250 + 3453)
+    train, dev, test = shuffle_data(all_data)
+    print(f'{len(train)} {len(dev)} {len(test)}')
+    print(train[0])
+    print()
+    print(dev[0])
+    print()
+    print(test[0])
+    data_to_file(train, Path(data_dir, 'new_train.txt'))
+    data_to_file(dev, Path(data_dir, 'new_dev.txt'))
+    data_to_file(test, Path(data_dir, 'new_test.txt'))
+    print('done')
 
 
-'''
-    @classmethod
-    def get_num_classes(cls, dataset: List[Dict[str, Any]]) -> int:
-        '''
-        Given the output of :py:func:`neural_ner.util.CoNLL.readCoNLL`
-        returns the number of NER classes in the dataset.
-        '''
-        class_data = []
-        for sentence in dataset:
-            class_data.extend(sentence['NER_BIO'])
-        return len(set(class_data))
-
-    @classmethod
-    def random_shuffle(cls, train: List[Dict[str, Any]], dev: List[Dict[str, Any]], 
-                       test: List[Dict[str, Any]], test_size: float = 0.2, 
-                       dev_size:float = 0.2, 
-                       random_state: 'np.random.RandomState' = np.random.RandomState()
-                       ) -> List[List[Dict[str, Any]]]:
-        '''
-        Combines the train, development and test data and then re-splits the 
-        data into different train, development and test data.
-        
-        :param train: The output of :py:func:`neural_ner.util.CoNLL.readCoNLL`
-        :param dev: The output of :py:func:`neural_ner.util.CoNLL.readCoNLL`
-        :param test: The output of :py:func:`neural_ner.util.CoNLL.readCoNLL`
-        :param test_size: Size of the test set as a fraction of all of the data
-        :param dev_size: Size of the development set as a fraction of the 
-                        train data (train data created after the initial data
-                        has been split into train and test splits)
-        :param random_state: Random state for splitting the data. Default 
-                            is completely random.
-        :return: A list of size 3 representing train, dev, and test data. 
-                Where the data is in the same format as the original input.
-        '''
-        num_classes = cls.get_num_classes(train)
-        all_data = train + dev + test
-        train, test = train_test_split(all_data, test_size=test_size,
-                                       random_state=random_state)
-        train, dev = train_test_split(train, test_size=dev_size,
-                                      random_state=random_state)
-        for dataset in [train, dev, test]:
-            if cls.get_num_classes(dataset) != num_classes:
-                return cls.random_shuffle(train, dev, test, test_size, dev_size,
-                                          random_state)
-        return train, dev, test
-'''
-            
